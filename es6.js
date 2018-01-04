d3b99e85b43adc9824b134b1a87046fef19ad673
jellytran80 <jellytran80@yahoo.com>
2018-01-04 10:11:13 +0700

    Update es6.js

--- es6.js
+++ es6.js
@@ -1,3 +1,19 @@
 //Javascript: find an object in array based on object's property
-
 let obj = objArray.find(function (obj) { return obj.id === 3; });
+
+// Number Sign Determination
+console.log(Math.sign(7)); // 1
+console.log(Math.sign(-7)); // -1
+console.log(Math.sign(0)); // 0
+console.log(Math.sign(-0)); // -0
+console.log(Math.sign(NaN)); // NaN
+
+// Number Type Checking
+Number.isNaN(7) //false
+Number.isNaN(NaN) // true
+Number.isFinite(NaN) // false
+Number.isFinite(7) // true
+Number.isFinite(Infinity) // false
+Number.isFinite(-Infinity) // false
+
+
